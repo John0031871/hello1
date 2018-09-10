4c3ca4ed766970e5fd97629b77624000f11a35e9
John Hammond <rc0031871@student.riversidecollege.ac.uk>
2018-09-10 15:35:09 +0100

    introduced bugs

--- hello1/Form1.cs
+++ hello1/Form1.cs
@@ -19,8 +19,8 @@ namespace hello1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello");
+            MessageBox.show("hello")
 
         }
-    }
+    
 }
